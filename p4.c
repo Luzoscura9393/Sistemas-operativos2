f796c581e4c2f0e53c02fcc7d73e94a3e184a49c
JuanCamilo268 <juleonba@unal.edu.co>
2025-10-17 12:08:21 -0500

    Update p4.c

--- p4.c
+++ p4.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
 
+int main(void) {
+  const char nombre[] = "/semaforo4";
+
+  sem_t *semaforo = sem_open(nombre, O_CREAT | O_RDWR, 0666, 0);
+
+  if (semaforo == SEM_FAILED) {
+    perror("sem_open fallo\n");
+    return 1;
+  }
+
+  sem_post(semaforo);
+
+  sleep(10);
+
+  sem_wait(semaforo);
+  sem_close(semaforo);
+  return 0;
+}
