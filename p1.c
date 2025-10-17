7bdd1687bf1dfaf689d116f30fbbeeb36f28f99f
JuanCamilo268 <juleonba@unal.edu.co>
2025-10-17 15:17:35 -0500

    Update p1.c

--- p1.c
+++ p1.c
@@ -5,8 +5,8 @@
 #include <semaphore.h>
 
 int main(int argc, char *argv[]) {
-  sem_t *semaforo = sem_open("/semaforo", 0666, 1);
-  sem_t *semaforo4 = sem_open("/semaforo4", 0666, 1);
+  sem_t *semaforo = sem_open("/semaforo", O_CREAT | O_RDWR, 0666, 0);
+  sem_t *semaforo4 = sem_open("/semaforo4", O_CREAT | O_RDWR, 0666, 0);
 
   if (semaforo == SEM_FAILED | semaforo4 == SEM_FAILED) {
     perror("sem_open fallo\n");
@@ -23,12 +23,24 @@ int main(int argc, char *argv[]) {
   } 
 
   if (valor > 0 && valor4 > 0) {
+    
     pid_t pid;
     int N = atoi(argv[1]);
     int a1 = atoi(argv[2]);
     int a2 = atoi(argv[3]);
     int a3 = atoi(argv[4]);
-  
+
+    sem_t *dato1 = sem_open("/dato1", 0666, 0);
+    sem_t *dato3 = sem_open("/dato3", O_CREAT | O_RDWR, 0666, 0);
+    const char maximo[] = "/tmp/miFifo";
+    unlink(maximo);
+    mkfifo(maximo, 0666);
+    int fd = open(maximo, O_WRONLY);
+    write(fd, &N, sizeof(int));
+    close(fd);
+    sem_post(dato1);
+    sem_wait(dato3);
+
     pid = fork();
 
     if (pid < 0) {
@@ -42,7 +54,16 @@ int main(int argc, char *argv[]) {
     return -2;
     }
     else {
-      printf("%d\n", a1);
+      sem_t *esperando1 = sem_open("/esperando1", 0666, -1);
+      sem_t *esperando4 = sem_open("/esperando4", O_CREAT | O_RDWR, 0666, 0);
+
+
+      int conexion = shmget(12345, sizeof(int), IPC_CREAT | 0666);
+      int *buffer = (int *)shmat(conexion, NULL, 0);
+      *buffer = a1;
+      sem_post(esperando1);
+      sem_wait(esperando4);
+      
       printf("%d\n", a2);
       for(int i = 0; i < N - 2; i ++){
         int j = a2;
