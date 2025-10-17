df7d1a1ef2cbcd3b93e4f201ad396a549b6a0758
JuanCamilo268 <juleonba@unal.edu.co>
2025-10-17 12:07:41 -0500

    Update p3.c

--- p3.c
+++ p3.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <semaphore.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <sys/ips.h>
+#include <sys/shm.h>
 
 int main(void) {
-  sem_t *semaforo;
-  int valor;
+  const char nombre[] = "/semaforo";
 
-  semaforo = sem_open("/tmp/semaforo", 0);
+  sem_t *semaforo = sem_open(nombre, O_CREAT | O_RDWR, 0666, 0);
 
   if (semaforo == SEM_FAILED) {
     perror("sem_open fallo\n");
     return 1;
   }
-  if (sem_getvalue(semaforo, &valor) == -1) {
-    perror("sem_getvaalue fallo\n");
-    sem_close(semaforo);
-    return 1;
-  }
-  
 
-  if (valor > 0) {
-    printf("Desactvado\n");
-  }
-  else {
-    printf("%d\n", valor);
-  }
+  sem_post(semaforo);
+
+  int conexion = shmget(12345, sizeof(int), IPC_CREAT | 0666);
+  int *buffer (int *)shmat(shmid, NULL, 0);
+
+  sem_wait(semaforo);
   sem_close(semaforo);
-  sem_unlink("/tmp/semaforo");
   return 0;
 }
