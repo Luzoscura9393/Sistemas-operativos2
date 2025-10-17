6f46fe881426c0fc35fd45d9c97cbb7f1dfe9d18
JuanCamilo268 <juleonba@unal.edu.co>
2025-10-17 15:19:55 -0500

    Update p4.c

--- p4.c
+++ p4.c
@@ -5,11 +5,15 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 int main(void) {
   const char nombre[] = "/semaforo4";
 
   sem_t *semaforo = sem_open(nombre, O_CREAT | O_RDWR, 0666, 0);
+  sem_t *esperando2 = sem_open("/esperando2", O_CREAT | O_RDWR, 0666, 0);
+  sem_t *esperando1 = sem_open("/esperando1", 0666, 0);
 
   if (semaforo == SEM_FAILED) {
     perror("sem_open fallo\n");
@@ -18,7 +22,22 @@ int main(void) {
 
   sem_post(semaforo);
 
-  sleep(10);
+  sem_t *dato2 = sem_open("/dato2", O_CREAT | O_RDWR, 0666, 0);
+  sem_t *dato3 = sem_open("/dato3", 0666, 0);
+  sem_wait(dato2);
+  const char maximo[] = "/tmp/miFifo";
+  int fd = open(maximo, O_RDONLY);
+  int N;
+  read(fd, &N, sizeof(int));
+  sem_post(dato3);
+
+  for(int i = 0; i < N; i++) {
+    sem_wait(esperando2);
+    printf("%d\n", *buffer);
+    sem_post(esperando1);
+  }
+
+
 
   sem_wait(semaforo);
   sem_close(semaforo);
