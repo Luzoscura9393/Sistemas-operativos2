bcdbd0eb134f4aff24e9ccadb73c87a351c8f041
JuanCamilo268 <juleonba@unal.edu.co>
2025-10-21 12:03:26 -0500

    Update p4.c

--- p4.c
+++ p4.c
@@ -12,7 +12,7 @@ int main(void) {
 
   sem_t *semaforo = sem_open(nombre, O_CREAT | O_RDWR, 0666, 0);
   sem_t *esperando2 = sem_open("/esperando2", O_CREAT | O_RDWR, 0666, 0);
-  sem_t *esperando1 = sem_open("/esperando1", 0666, 0);
+  sem_t *esperando4 = sem_open("/esperando4", 0666, 0);
 
   if (semaforo == SEM_FAILED) {
     perror("sem_open fallo\n");
@@ -24,11 +24,11 @@ int main(void) {
   sem_t *dato2 = sem_open("/dato2", O_CREAT | O_RDWR, 0666, 0);
   sem_t *dato3 = sem_open("/dato3", 0666, 0);
   const char maximo[] = "/tmp/miFifo";
+  printf("Esperando por P2\n");
   int fd = open(maximo, O_RDONLY);
   int N;
   read(fd, &N, sizeof(int));
   close(fd);
-  printf("Esperando por P2\n");
   sem_wait(dato2);
   
   
@@ -41,21 +41,13 @@ int main(void) {
     perror("Error abriendo memoria compartida");
     return 1;
   }
-
-
   int *buffer = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (buffer == MAP_FAILED) {
-    perror("Error en mmap");
-    close(ft);
-    return 1;
-  }
-
   for(int i = 0; i < N; i++) {
-    sem_wait(esperando2);
-    printf("%d\n", *buffer);
-    sem_post(esperando1);
+  sem_wait(esperando2);
+  printf("%d\n", *buffer);
+  sem_post(esperando4);
   }
-
+  printf("P4 termina\n");
   sem_wait(semaforo);
   sem_close(semaforo);
   return 0;
