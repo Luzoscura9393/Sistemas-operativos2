15ec2c6360fed5c165bc91500d376bd1d7f96864
Luzoscura9393 <anloaizag@unal.edu.co>
2025-10-24 16:14:01 -0500

    Update p3.c

--- p3.c
+++ p3.c
@@ -11,32 +11,56 @@ int main(void) {
   const char nombre[] = "/semaforo";
 
   sem_t *semaforo = sem_open(nombre, O_CREAT | O_RDWR, 0666, 0);
-  sem_t *esperando1 = sem_open("/esperando1", O_CREAT | O_RDWR, 0666, 0);
-  sem_t *esperando3 = sem_open("/esperando3", 0666, 0);
-
 
   if (semaforo == SEM_FAILED) {
     perror("sem_open fallo\n");
     return 1;
   }
-
   sem_post(semaforo);
+
+  sem_t *esperando1 = sem_open("/esperando1", O_CREAT | O_RDWR, 0666, 0);
+  sem_t *esperando3 = sem_open("/esperando3", O_CREAT | O_RDWR, 0666, 0);  
   
+  if (esperando1 == SEM_FAILED) {
+    perror("Error al abrir semáforo dato1\n");
+    exit(1);
+  }
+  if (esperando3 == SEM_FAILED) {
+    perror("Error al abrir semáforo dato3\n");
+    exit(1);
+  }
+
   sem_t *dato1 = sem_open("/dato1", O_CREAT | O_RDWR, 0666, 0);
-  sem_t *dato2 = sem_open("/dato2", 0666, 0);
+  sem_t *dato2 = sem_open("/dato2", O_CREAT | O_RDWR, 0666, 0);
+
+  if (dato1 == SEM_FAILED) {
+    perror("Error al abrir semáforo dato1\n");
+    exit(1);
+  }
+  if (dato2 == SEM_FAILED) {
+    perror("Error al abrir semáforo dato2\n");
+    exit(1);
+  }
+
   const char maximo[] = "/tmp/miFifo";
   printf("Esperando por P1\n");
+
   int fd = open(maximo, O_RDONLY);
+  if (fd == -1) {
+    perror("Error abriendo FIFO /tmp/miFifo en P3");
+    exit(1);
+  }
+
   int N;
-  read(fd, &N, sizeof(int));
-  close(fd);
-  
-  sem_wait(dato1);
-  
-  sem_post(dato2);
+  if (read(fd, &N, sizeof(int)) != sizeof(int)) {
+    perror("Error leyendo N desde FIFO /tmp/miFifo en P3");
+    close(fd);
+    exit(1);
+  }  
 
 
   const char *nombre_memoria = "/mem_compartida";
+
   int ft = shm_open(nombre_memoria, O_CREAT | O_RDWR, 0666);
   if(ft == -1){
   perror("ERROR EN SHM_OPEN");
@@ -49,14 +73,42 @@ int main(void) {
   }
   
   int *buffer = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, ft, 0);
-  for(int i = 0; i < N; i++) {
-    sem_wait(esperando1);
+  for (int i = 0; i < N + 1; i++) {
+      sem_wait(esperando1);
+
+      if (*buffer == -1) {  
+        printf("P3 recibió testigo -1\n");
+        sem_post(esperando3);
+        
+        const char *fifo_conf = "/tmp/p1_confirm";
+        if (mkfifo(fifo_conf, 0666) == -1 && errno != EEXIST) {
+        perror("P3: mkfifo /tmp/p1_confirm falló");
+        }
+        int wfd = open(fifo_conf, O_WRONLY);
+        if (wfd != -1) {
+            int confirm = -3;
+            if (write(wfd, &confirm, sizeof(int)) != sizeof(int)) {
+                perror("P3: error escribiendo confirmacion en FIFO");
+            }
+            close(wfd);
+        } else {
+             perror("P3: error abriendo /tmp/p1_confirm para escritura");
+        }
+
+      break;  
+    }    
+
     printf("%d\n", *buffer);
     sem_post(esperando3);
-  }
+}
 
   printf("P3 termina\n");
-  sem_wait(semaforo);
+  munmap(buffer, sizeof(int));
+  close(ft); 
+  sem_close(esperando1);
+  sem_close(esperando3);
+  sem_unlink("/esperando1");
+  sem_unlink("/esperando3");
   sem_close(semaforo);
   return 0;
 }
